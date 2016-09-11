85383092d4926c509ea16c7f9490faf4f823afcb
jigkoh3 <jigkoh3@hotmail.com>
2016-09-11 09:46:17 +0700

    install express and config

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+
+//middleware
+var helloworld = function(req,res,next){
+	res.send('hello server');
+}
+
+//use middleware
+//app.use('/',helloworld);
+
+//config server port
+app.listen(3000);
+console.log('Server running at http://localhost:3000');
